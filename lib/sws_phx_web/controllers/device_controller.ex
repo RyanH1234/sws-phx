28368bcf9164920ac47f69bfaf4aac0b11429442
Ryan <hodgsonr1234@gmail.com>
2020-09-22 17:48:19 +0800

    (secret key) added in secret key for device to send payload

--- lib/sws_phx_web/controllers/device_controller.ex
+++ lib/sws_phx_web/controllers/device_controller.ex
@@ -14,25 +14,35 @@ defmodule SwsPhxWeb.DeviceController do
       }
   end
 
-  def insert_device_data(conn, %{"timestamp" => timestamp, "type" => type_description, "value" => value, "device_id" => did} = params) do
-    # debug
-    params |> IO.inspect()
+  def insert_device_data(conn, %{
+        "token" => token,
+        "timestamp" => timestamp,
+        "type" => type_description,
+        "value" => value,
+        "device_id" => did
+      }) do
+    secret_key = Application.get_env(:sws_phx, SwsPhx.Guardian)[:secret_key]
 
-    # look up database to get device_data_type id
-    %{id: type_id} = get_type_id(type_description) |> Repo.one()
+    case secret_key == token do
+      true ->
+        # look up database to get device_data_type id
+        %{id: type_id} = get_type_id(type_description) |> Repo.one()
 
-    # create payload
-    params = %{
-      device_data_type_id: type_id,
-      value: value,
-      device_id: did,
-      timestamp: timestamp
-    } |> IO.inspect()
+        # create payload
+        params = %{
+          device_data_type_id: type_id,
+          value: value,
+          device_id: did,
+          timestamp: timestamp
+        }
 
-    # insert into database
-    device_data_changeset = DeviceData.changeset(%DeviceData{}, params)
-    Repo.insert!(device_data_changeset)
+        # insert into database
+        device_data_changeset = DeviceData.changeset(%DeviceData{}, params)
+        Repo.insert!(device_data_changeset)
+        json(conn, "success")
 
-    json(conn, "success")
+      false ->
+        json(conn, "error")
+    end
   end
 end
